e1899cd798ee0fa15050d2bb4c895838647b5890
peter <peter_batchelor1@hotmail.com>
2023-09-21 11:53:59 +0100

    Updating

--- camera_chess/constants.py
+++ camera_chess/constants.py
@@ -7,7 +7,6 @@ STUDIO_IMAGE_DIR = os.path.join(STUDIO_DIR, 'files', 'images')
 STUDIO_LABEL_DIR = os.path.join(STUDIO_DIR, 'files', 'labels')
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 YOLO_DIR = os.path.join(DATA_DIR, 'yolo')
-CLASSIFIER_DIR = os.path.join(DATA_DIR, 'classifier')
 KEYPOINTS_DIR = os.path.join(DATA_DIR, 'keypoints')
 
 CLASS_TO_PIECE = {'black-bishop': chess.Piece(chess.BISHOP, chess.BLACK),
@@ -21,13 +20,11 @@ CLASS_TO_PIECE = {'black-bishop': chess.Piece(chess.BISHOP, chess.BLACK),
                   'white-knight': chess.Piece(chess.KNIGHT, chess.WHITE),
                   'white-pawn': chess.Piece(chess.PAWN, chess.WHITE),
                   'white-queen': chess.Piece(chess.QUEEN, chess.WHITE),
-                  'white-rook': chess.Piece(chess.ROOK, chess.WHITE),
-                  'empty': 'empty'}
+                  'white-rook': chess.Piece(chess.ROOK, chess.WHITE)}
 PIECE_TO_CLASS = {v: k for k, v in CLASS_TO_PIECE.items()}
 CLASSES = list(CLASS_TO_PIECE.keys())
 CORNERS = ['h1', 'a1', 'a8', 'h8']
 SQUARE_SIZE = 128
-MARGIN = 3 * SQUARE_SIZE
 BOARD_SIZE = 8 * SQUARE_SIZE
 COLOUR_MAP = {'black-pawn': 'white',
               'white-pawn': 'white',
@@ -52,8 +49,7 @@ ABBR_MAP = {'black-pawn': 'p',
             'black-king': 'k',
             'white-king': 'K',
             'black-queen': 'q',
-            'white-queen': 'Q',
-            'empty': ''}
+            'white-queen': 'Q'}
 SQUARE_TO_PIECE = {**{f'{i}2': 'white-pawn' for i in 'abcdefgh'},
                    **{f'{i}7': 'black-pawn' for i in 'abcdefgh'},
                    'a1': 'white-rook',
--- tracker.py
+++ tracker.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import os
+import sys
 
 import chess
 import cv2
@@ -7,41 +9,25 @@ import matplotlib.cm as cmx
 import matplotlib.colors as colors
 import numpy as np
 from PIL import Image, ImageDraw
+from icecream import ic
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
-from camera_chess.constants import BOARD_SIZE, SQUARE_SIZE, CLASSES, ABBR_MAP, SQUARE_TO_PIECE, PIECE_TO_CLASS
+from camera_chess.constants import CLASSES, ABBR_MAP, PIECE_TO_CLASS
 from camera_chess.detector import Detector
 from camera_chess.utils import load_video_config
 from camera_chess.video import Video
 
-
-def _xy_to_square(x, y):
-    return f'{chr(x + 97)}{8 - y}'
-
-
-def _square_to_xy(square):
-    x = ord(square[0]) - 97
-    y = 8 - int(square[1])
-    return x, y
-
-
-def _init_state():
-    state = np.zeros((8, 8, len(CLASSES)), dtype=np.float32)
-    for square, piece in SQUARE_TO_PIECE.items():
-        x, y = _square_to_xy(square)
-        cls_idx = CLASSES.index(piece)
-        state[x][y][cls_idx] = 1.0
-    return state
+sys.path.append('../CameraChessWeb')
+from aws.tracker.lambda_function import get_centers_and_boundary
 
 
 def _update_state(state, arr, decay=0.5):
     state *= decay
-    x = arr[:, 0].astype(np.int32)
-    y = arr[:, 1].astype(np.int32)
-    conf = arr[:, 2]
-    cls = arr[:, 3].astype(np.int32)
-    state[x, y, cls] += (1 - decay) * conf
+    square = arr[:, 0].astype(np.int32)
+    conf = arr[:, 1]
+    cls = arr[:, 2].astype(np.int32)
+    state[square, cls] += (1 - decay) * conf
 
 
 class Creator:
@@ -52,28 +38,15 @@ class Creator:
         self.video_config = load_video_config(self.dataset)
         self.video = Video(self.video_config, target_fps=8)
 
-        self.square_centers = self._warp((np.mgrid[0:8, 0:8].reshape(2, -1).T + 0.5) * SQUARE_SIZE)
-        self.boundary = self._warp(np.array([[-0.5, -0.5], [-0.5, 8.5], [8.5, 8.5], [8.5, -0.5]]) * SQUARE_SIZE)
+        self.centers, self.boundary = get_centers_and_boundary(self.video.new_keypoints)
 
         cmap = plt.get_cmap('Blues')
         norm = colors.Normalize(vmin=0.0, vmax=1.0)
         self.scalar_map = cmx.ScalarMappable(norm=norm, cmap=cmap)
 
-    @staticmethod
-    def _perspective_transform(src, matrix):
-        homo_src = src.transpose(1, 0, 2)
-        homo_src = np.concatenate([homo_src, np.ones((len(homo_src), 1, 1))], axis=2)
-        warped_src = homo_src @ matrix.T
-        warped_src /= warped_src[..., 2:]
-        warped_src = warped_src[..., :2]
-        warped_src = warped_src.transpose(1, 0, 2)
-        return warped_src
-
-    def _get_nearest_xy(self, center):
-        idx = np.argmin(np.sum((center - self.square_centers) ** 2, axis=1))
-        x = idx // 8
-        y = idx % 8
-        return x, y
+    def _get_nearest_square(self, center):
+        square = np.argmin(np.sum((center - self.centers) ** 2, axis=1))
+        return square
 
     def _is_out_of_bounds(self, center):
         for i in range(4):
@@ -84,51 +57,32 @@ class Creator:
                 return True
         return False
 
-    def _warp(self, src):
-        target = np.array([[BOARD_SIZE, BOARD_SIZE],
-                           [0, BOARD_SIZE],
-                           [0, 0],
-                           [BOARD_SIZE, 0]], dtype=np.float32)
-        matrix = self._get_perspective_transform(target)
-        inv_matrix = np.linalg.inv(matrix)
-        warped_src = self._perspective_transform(np.expand_dims(src, axis=0), inv_matrix)[0]
-        return warped_src
-
-    def _get_perspective_transform(self, target):
-        A = np.zeros((8, 8), dtype=np.float32)
-        B = np.zeros((8, 1), dtype=np.float32)
-
-        for i in range(4):
-            x, y = self.video.new_keypoints[i]
-            u, v = target[i]
-            A[i * 2] = [x, y, 1, 0, 0, 0, -u * x, -u * y]
-            A[i * 2 + 1] = [0, 0, 0, x, y, 1, -v * x, -v * y]
-            B[i * 2] = u
-            B[i * 2 + 1] = v
-
-        matrix = np.linalg.solve(A, B)
-        matrix = np.append(matrix, 1.0)
-        matrix = matrix.reshape((3, 3))
-        return matrix
-
-    def _plot_state(self, state, thr=0.2):
+    def _plot_state(self, state, from_square, to_square, thr=0.2):
         size = (8 * self.PLOT_SIZE + 1, 8 * self.PLOT_SIZE + 1)
         image = Image.new('RGB', size)
         d = ImageDraw.Draw(image)
-        max_idx = np.argmax(state, axis=2)
-        for x in range(8):
-            for y in range(8):
-                cls = max_idx[x][y]
-                score = state[x][y][cls]
+        max_idx = np.argmax(state, axis=1)
+        for square in range(64):
+            cls = max_idx[square]
+            score = state[square][cls]
 
-                bbox = [self.PLOT_SIZE * i for i in [x, y, (x + 1), (y + 1)]]
-                fill = colors.rgb2hex(self.scalar_map.to_rgba(score))
-                d.rectangle(bbox, fill=fill, outline="black")
+            x = square % 8
+            y = 7 - (square // 8)
+            bbox = [self.PLOT_SIZE * i for i in [x, y, (x + 1), (y + 1)]]
+            fill = colors.rgb2hex(self.scalar_map.to_rgba(score))
 
-                if score > thr:
-                    d.text([self.PLOT_SIZE * (x + 0.45), self.PLOT_SIZE * (y + 0.45)],
-                           ABBR_MAP[CLASSES[cls]],
-                           fill='black')
+            if square in {from_square, to_square}:
+                outline = "yellow"
+                width = 5
+            else:
+                outline = "black"
+                width = 1
+            d.rectangle(bbox, fill=fill, outline=outline, width=width)
+
+            if score > thr:
+                d.text([self.PLOT_SIZE * (x + 0.45), self.PLOT_SIZE * (y + 0.45)],
+                       ABBR_MAP[CLASSES[cls]],
+                       fill='black')
         return image
 
     def create_sequence(self, sequence_path):
@@ -148,147 +102,177 @@ class Creator:
                 if oob:
                     continue
 
-                x, y = self._get_nearest_xy(center)
-                new_pred = [i, x, y, pred[-2], pred[-1]]
+                square = self._get_nearest_square(center)
+                new_pred = [i, square, pred[-2], pred[-1]]
                 sequence.append(new_pred)
 
         np.save(sequence_path, sequence)
 
-    def create_video(self, sequence_path):
+    def create_video(self, sequence_path, video_path, logs_path=None):
         sequence = np.load(sequence_path)
+
+        if logs_path is not None:
+            with open(logs_path, 'rb') as f:
+                logs = json.load(f)
+        else:
+            logs = {}
 
         size = (8 * self.PLOT_SIZE + 1, 8 * self.PLOT_SIZE + 1)
         fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        writer = cv2.VideoWriter('video.mp4', fourcc, self.video.target_fps, size)
+        writer = cv2.VideoWriter(video_path, fourcc, self.video.target_fps, size)
 
-        state = _init_state()
+        state = np.zeros((64, len(CLASSES)), dtype=np.float32)
         idxs, breakpoints = np.unique(sequence[:, 0], return_index=True)
         arrs = np.split(sequence[:, 1:], breakpoints[1:])
+        from_square = None
+        to_square = None
         for idx, arr in tqdm(zip(idxs, arrs), total=len(idxs)):
             _update_state(state, arr)
 
-            image = self._plot_state(state)
+            idx = str(idx)
+            if idx in logs:
+                candidates = logs[idx]
+                best_candidate = max(candidates, key=lambda x: x["score"])
+                board = chess.Board()
+                for san_move in best_candidate["san"]:
+                    uci_move = board.parse_san(san_move)
+                    board.push(uci_move)
+                    from_square = uci_move.from_square
+                    to_square = uci_move.to_square
+
+            image = self._plot_state(state, from_square, to_square)
             data = np.array(image)
             writer.write(data[..., ::-1])
 
         writer.release()
 
 
-class Candidate:
-    MOVE_PENALTY = 2.5
+class Tracker:
+    def _get_from_and_to_squares(self, move, board):
+        from_squares = []
+        to_squares = []
+        if board.is_castling(move) or board.is_en_passant(move):
+            next_board = board.copy(stack=False)
+            next_board.push(move)
+            for square in range(64):
+                board_piece = PIECE_TO_CLASS.get(board.piece_at(square), None)
+                next_board_piece = PIECE_TO_CLASS.get(next_board.piece_at(square), None)
+                if board_piece == next_board_piece:
+                    continue
 
-    def __init__(self):
-        self.board = chess.Board()
-        self.san_stack = []
-        self.score = 0
+                if next_board_piece is None:
+                    from_squares.append(square)
 
-    def calculate_hash(self):
-        return hash(self.board.fen().split(' ', 1)[0])
+                if board_piece is None:
+                    to_squares.append(square)
+        else:
+            from_squares.append(move.from_square)
+            to_squares.append(move.to_square)
+        return from_squares, to_squares
 
-    def calculate_score(self, state, move):
-        probs = np.zeros(64, dtype=np.float32)
-        piece_map = self.board.piece_map()
-        for square in range(64):
-            piece = piece_map.get(square, None)
-            x, y = _square_to_xy(chess.square_name(square))
-            if piece is None:
-                p = 1 - max(state[x][y])
-            else:
-                cls = CLASSES.index(PIECE_TO_CLASS[piece])
-                p = state[x][y][cls]
-            probs[square] = p
-        score = np.sum(np.log(probs + 0.01))
-        if move is not None:
-            score -= self.MOVE_PENALTY
+    def _get_squares(self, move_1, board, move_2=None):
+        from_squares_1, to_squares_1 = self._get_from_and_to_squares(move_1, board)
+        if move_2 is None:
+            return from_squares_1, to_squares_1
+
+        board.push(move_1)
+        from_squares_2, to_squares_2 = self._get_from_and_to_squares(move_2, board)
+        board.pop()
+
+        squares_2 = from_squares_2 + to_squares_2
+        from_squares = list(set(from_squares_1) - set(squares_2)) + from_squares_2
+        to_squares = list(set(to_squares_1) - set(squares_2)) + to_squares_2
+
+        return from_squares, to_squares
+
+    def _calculate_score(self, state, move_1, board, move_2=None):
+        from_squares, to_squares = self._get_squares(move_1, board, move_2)
+
+        score = 0
+        for square in from_squares:
+            score += 1 - max(state[square]) - 0.7
+
+        board.push(move_1)
+        if move_2 is not None:
+            board.push(move_2)
+
+        for square in to_squares:
+            cls = CLASSES.index(PIECE_TO_CLASS[board.piece_at(square)])
+            score += state[square][cls] - 0.7
+        board.pop()
+        if move_2 is not None:
+            board.pop()
+
         return score
 
-    def push(self, move):
-        if move is not None:
-            self.san_stack.append(self.board.san(move))
-            self.board.push(move)
-
-    def pop(self, move):
-        if move is not None:
-            self.san_stack.pop()
-            self.board.pop()
-
-    def __str__(self):
-        s = f'{self.score:.2f} '
-        for i, move in enumerate(self.san_stack):
-            if i % 2 == 0:
-                s += f'{(i + 2) // 2}. {str(move)}'
-            else:
-                s += f' {str(move)} '
-        return s
-
-
-class Tracker:
-    def _process_point(self, state, arr, new_candidates):
+    def _process_point(self, state, arr, board, moves):
         _update_state(state, arr)
 
-        old_candidates = new_candidates
-        new_candidates = []
-        seen = set()
-        for candidate in old_candidates:
-            moves = list(candidate.board.legal_moves) + [None]
-            for move in moves:
-                candidate.push(move)
+        if not len(moves):
+            for move_1 in board.legal_moves:
+                board.push(move_1)
+                for move_2 in board.legal_moves:
+                    moves.append([move_1, move_2])
+                board.pop()
 
-                hash_ = candidate.calculate_hash()
-                if hash_ not in seen:
-                    seen.add(hash_)
+        best_moves = []
+        best_score = -float('inf')
+        for move_1, move_2 in moves:
+            board.push(move_1)
+            move_2_score = self._calculate_score(state, move_2, board)
+            board.pop()
 
-                    score = candidate.calculate_score(state, move)
+            move_1_score = self._calculate_score(state, move_1, board)
+            joint_score = self._calculate_score(state, move_1, board, move_2)
 
-                    valid = False
-                    if len(new_candidates) < 3:
-                        valid = True
-                    elif score > new_candidates[-1].score:
-                        new_candidates.pop()
-                        valid = True
+            if move_2_score < 0 or joint_score < 0:
+                continue
 
-                    if valid:
-                        new_candidate = Candidate()
-                        new_candidate.san_stack = candidate.san_stack[:]
-                        new_candidate.board = candidate.board.copy(stack=False)
-                        new_candidate.score = score
-                        new_candidates.append(new_candidate)
+            ic(move_1, move_2, move_1_score, move_2_score, joint_score)
 
-                    new_candidates.sort(key=lambda x: x.score, reverse=True)
-                candidate.pop(move)
+            if joint_score > best_score:
+                best_score = joint_score
+                best_moves = [move_1, move_2]
+                ic(best_score, best_moves)
 
-        return new_candidates
+        return best_moves, best_score
 
-    def process_sequence(self, sequence_path):
+    def process_sequence(self, sequence_path, logs_path):
         sequence = np.load(sequence_path)
 
-        logs = []
-        state = _init_state()
-        init_candidate = Candidate()
-        init_candidate.score = init_candidate.calculate_score(state, None)
-        new_candidates = [init_candidate]
+        logs = {}
+        state = np.zeros((64, len(CLASSES)), dtype=np.float32)
+        board = chess.Board()
+        moves = []
 
         idxs, breakpoints = np.unique(sequence[:, 0], return_index=True)
         arrs = np.split(sequence[:, 1:], breakpoints[1:])
         for idx, arr in tqdm(zip(idxs, arrs), total=len(idxs)):
-            new_candidates = self._process_point(state, arr, new_candidates)
+            best_moves, best_score = self._process_point(state, arr, board, moves)
 
-            if idx % 10 == 0:
-                for candidate in new_candidates:
-                    logs.append(f'{idx} {candidate}')
+            if best_score > 0:
+                board.push(best_moves[0])
+                logs[idx] = {"best_moves": ' '.join([move.uci() for move in best_moves]),
+                             "best_score": best_score,
+                             "stack": ' '.join([move.uci() for move in board.move_stack])}
+                moves = []
 
-        with open('logs.txt', 'w') as f:
-            f.write('\n'.join(logs))
+        with open(logs_path, 'w') as f:
+            json.dump(logs, f, indent=2)
 
 
 def main(dataset):
     sequence_path = f'{dataset.replace("/", "_")}_sequence.npy'
+    logs_path = f'{os.path.splitext(os.path.basename(sequence_path))[0]}_logs.json'
+    video_path = f'{os.path.splitext(os.path.basename(sequence_path))[0]}_video.mp4'
 
     creator = Creator(dataset)
     creator.create_sequence(sequence_path)
 
     tracker = Tracker()
-    tracker.process_sequence(sequence_path)
+    tracker.process_sequence(sequence_path, logs_path)
+
+    # creator.create_video(sequence_path, video_path, logs_path)
 
 
 if __name__ == '__main__':
