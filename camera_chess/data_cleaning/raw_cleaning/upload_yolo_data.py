ce299510fac93106db679bde52ae0e8f43ddc490
peter <peter_batchelor1@hotmail.com>
2024-07-28 23:04:07 +0100

    Updaitng

--- camera_chess/data_cleaning/raw_cleaning/upload_yolo_data.py
+++ camera_chess/data_cleaning/raw_cleaning/upload_yolo_data.py
@@ -14,7 +14,7 @@ from camera_chess.utils import clear_dir
 
 
 def main():
-    dataset_id = 'dilanya'
+    dataset_id = 'kn'
     image_paths = sorted(glob(os.path.join(DATA_DIR, 'roboflow', str(dataset_id), 'images', '*.jpg')))
     clear_dir(STUDIO_IMAGE_DIR)
     clear_dir(STUDIO_LABEL_DIR)
